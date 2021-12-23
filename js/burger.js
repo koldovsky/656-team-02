1ccfe6e38df0889a8a7eb348023d67cd6e1bd87f
Dariya Dariya <daryakac86@gmail.com>
2021-12-23 17:25:51 +0200

    Ad js burger menu

---
+++ js/burger.js
@@ -0,0 +1,8 @@
+(function () {
+    document.querySelector('.burger_menu').addEventListener('click', function () {
+        document.querySelector('.burger_menu span').classList.toggle('.burger_menu span .active');
+    })
+
+
+
+})
