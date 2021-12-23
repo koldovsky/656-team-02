343fb5a2390781e5fa0174ccebc3d426f035cd64
Dariya Dariya <daryakac86@gmail.com>
2021-12-23 20:47:41 +0200

    js

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,5 @@
 (function () {
     document.querySelector('.burger_menu').addEventListener('click', function () {
-        document.querySelector('.burger_menu span').classList.toggle('.burger_menu span .active');
+        document.querySelector('.burger_menu span').classList.toggle('.active');
     })
-
-
-
-})
+})();
