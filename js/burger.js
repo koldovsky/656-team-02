f82f2a068daa457c65b2cbc0a008749e8f55270c
Dariya Dariya <daryakac86@gmail.com>
2021-12-24 12:28:03 +0200

    Burger menu ok!

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,8 @@
 (function () {
-    document.querySelector('.burger_menu').addEventListener('click', function() {
+    document.querySelector('.burger_menu').addEventListener('click', function () {
         document.querySelector('.burger_menu span').classList.toggle('active');
-    })
+        document.querySelector('.subheader_navigation_icons').classList.toggle('animate');
+    });
+
+    
 })();
