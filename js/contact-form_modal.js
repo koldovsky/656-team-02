659ec9b654a7f4b0290934b42aee85933ee68be8
IrynaDobrovolska <ua.green@gmail.com>
2021-12-27 12:29:23 +0000

    changed modal.css

--- js/contact-form_modal.js
+++ js/contact-form_modal.js
@@ -1,29 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let requestButton = document.getElementsByClassName('botton_request');
+  let requestButton = document.getElementsByClassName('.botton_request');
 
-  let modal1 = document.getElementById('modal_form');
+  let modal1 = document.getElementByClassName('.modal');
+  
+  let closeButton = modal1.getElementsByClassName('.modal__close-button_ok')[0];
 
-  let closeButton = modal1.getElementsByClassName('modal__close-button', 'modal__close-button_ok')[0];
-
-  let tagBody = document.getElementsByTagName('body');
 
   requestButton.onclick = function (e) {
     e.preventDefault();
     modal1.classList.add('modal_active');
-    tagBody.classList.add('hidden');
   }
 
   closeButton.onclick = function (e) {
     e.preventDefault();
     modal1.classList.remove('modal_active');
-    tagBody.classList.remove('hidden');
   }
 
-  modal1.onmousedown = function (e) {
-    let target = e.target;
-    let modalContent = modal1.getElementsByClassName('modal__content')[0];
-    if (e.target.closest('.' + modalContent.className) === null) {
-      this.classList.remove('modal_active');
-      tagBody.classList.remove('hidden');
-    }
-  };
+ 
